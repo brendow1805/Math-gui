14a62ecb5cc8aa1bfe8b729ec2532aace15700c6
00julia <130668482+00julia@users.noreply.github.com>
2023-10-25 09:07:59 -0300

    Commit math

--- math gui.py
+++ math gui.py
@@ -1,6 +1,36 @@
 #interface gráfica
 
+from Lógica import *
 import PySimpleGUI as psg
 
-layout = [[psg.Text()]]
+layou_frame = [
+    [psg.Radio('Tabuada: ', 'btnRadio1', key='tabuada')],
+    [psg.Radio('Fatorial: ', 'btnRadio1', key='fatorial', default=True)],
 
+]
+
+layout = [
+    [psg.Text('Informe um número: '), psg.InputText(key='numero'), psg.Frame('Opções: ', layou_frame)],
+    [psg.Text("", key='resultado')],
+    [psg.Button('Calcular'), psg.Button('Limpar')],
+]
+
+window = psg.Window('Sistema Matemático do Senai', layout)
+
+while True:
+    evento, valor = window.read()
+
+    if evento == psg.WIN_CLOSED:
+        break
+    else:
+        if valor['fatorial']:
+            num = gera_fatorial(int(valor['numero']))
+            window['resultado'].update(f'{valor["numero"]}! = {num}')
+        else:
+            #num = gerar_tabuada(int(valor['numero']))
+            # window['resultado'].update(f'{valor["numero"]}! = {num}')
+            window['resultado'].update(gerar_tabuada(int(valor['numero'])))
+
+
+
+window.close()
